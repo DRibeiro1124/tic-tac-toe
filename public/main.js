7c42e82ea591844e248710b305a721b1425b5af0
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-13 13:07:27 -0400

    added markdown page

--- public/main.js
+++ public/main.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', main)
-const main = () => {
-
-}
+const winningCombo = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+player1 = []
+player2 = []
